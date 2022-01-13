3e372e31ba48ef3eec714fe845239446a9fcf1fe
wvaras77 <97548785+wvaras77@users.noreply.github.com>
2022-01-13 09:21:52 -0300

    Se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,27 @@
+// const http = require('http');
+const colors = require('colors');
+
+
+// const handleServer = function(req,res){
+//     res.writeHead(200, { 'Content-type': 'text/html'});
+//     res.write('<h1>Hola mundo</h1>');
+//     res.end();
+// }
+
+// const server = http.createServer(handleServer);
+
+// server.listen(3000, function(){
+//     console.log('Server on port 3000'.rainbow);
+// });
+
+const express = require('express');
+const res = require('express/lib/response');
+const server = express();
+
+server.get('/', (req,res) =>{
+    res.send('<h1>Hola mundo con express y node</h1>');
+})
+
+server.listen(3000, () => {
+    console.log('Server on port 3000'.rainbow);
+})
